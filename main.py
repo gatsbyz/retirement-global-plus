43bb0955352004ffbc04e3f4ddd0b4c5a4c17d4e
Jesse Lee <jesse@titanvest.com>
2022-05-12 00:50:50 -0400

    pull together all code. meet end to end

--- main.py
+++ main.py
@@ -1,62 +1,20 @@
-# Import the required libraries and dependencies
-import os
-import requests
-import json
-import pandas as pd
-from dotenv import load_dotenv
 
-# Load the environment variables from the .env file by calling the load_dotenv function
-load_dotenv()
+import get_user_data
+import get_cost_of_living_data
+import simulation
 
-COST_OF_LIVING_URL = "https://cities-cost-of-living1.p.rapidapi.com/get_cities_details_by_name"
-COST_OF_LIVING_HEADERS = {
-	"content-type": "application/x-www-form-urlencoded",
-	"X-RapidAPI-Host": "cities-cost-of-living1.p.rapidapi.com",
-	"X-RapidAPI-Key": "0733542605msh08db76108a18425p16aaa1jsn85b280209dcc"
-}
-payload = {
-  'cities': '[{"name":"Hamilton","country":"Bermuda"},{"name":"New York","country":"United States"},{"name":"Hong Kong","country":"Hong Kong"},{"name":"Los Angeles","country":"United States"},{"name":"Paris","country":"France"},{"name":"Hamilton","country":"Canada"},{"name":"Milan","country":"Italy"},{"name":"Bucaramanga","country":"Colombia"},{"name":"Madrid","country":"Spain"},{"name":"Delhi","country":"India"}]',
-  'currencies': '["USD"]'
-}
+retirement_years, years_to_retirement, savings, portfolio_type, total_stocks_bonds = get_user_data.run()
+data, cities = get_cost_of_living_data.cost_of_living_data()
 
-# response = requests.request("POST", COST_OF_LIVING_URL, data=payload, headers=COST_OF_LIVING_HEADERS)
+savings_df = simulation.create_savings_dataframe(savings, total_stocks_bonds)
+df_portfolio = simulation.get_portfolio_dataframe()
 
-# rawData = response.json()['data']
+output = simulation.run_mc_output(df_portfolio, portfolio_type, years_to_retirement)
 
-# print(rawData)
+mean_cumulative_return = simulation.mean_cumulative_return(output, total_stocks_bonds)
 
-# hard coded data will replace when need to make real call
-f = open('exampleData.json')
-rawData = json.load(f)
-f.close()
+savings_at_retirement = simulation.savings_at_retirement(mean_cumulative_return, savings)
 
+retirement_cities = simulation.get_retirement_cities(output, data, retirement_years, total_stocks_bonds, savings)
 
-data = []
-cities = []
-
-for item in rawData:
-	cities.append(item['name'])
-
-	cityData = {}
-	cityData['city'] = item['name']
-	cityData['country'] = item['country']
-	cityData['country'] = item['country']
-	cityData['rent_index'] = float(item['rent_index'])
-	cityData['restaurant_price_index'] = float(item['restaurant_price_index'])
-	cityData['groceries_index'] = float(item['groceries_index'])
-	cityData['cost_of_living_index'] = float(item['cost_of_living_index'])
-	cityData['cost_of_living_plus_rent_index'] = float(item['cost_of_living_plus_rent_index'])
-	cityData['local_purchasing_power_index'] = float(item['local_purchasing_power_index'])
-
-	cost_of_living = item['cost_of_living_details'][0]['details']
-	estimated_cost_without_rent = next(x for x in cost_of_living if x['Item'] == "Estimated Monthly Costs Without Rent")
-	rent = next(x for x in cost_of_living if x['Item'] == "Apartment (1 bedroom) Outside of Centre")
-
-	cityData['estimated_monthly_costs_with_rent'] = float(estimated_cost_without_rent['Value']) + float(rent['Value'])
-	cityData['estimated_monthly_costs_without_rent'] = float(estimated_cost_without_rent['Value'])
-
-	data.append(cityData)
-
-
-print(json.dumps(data, indent=4, sort_keys=True))
-print(cities)
+print(retirement_cities)
